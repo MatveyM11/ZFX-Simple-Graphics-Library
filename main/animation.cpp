ec67d076fe6fe0b7c7607cca6153d0db7e83a952
Matvey Mochalov <tutaitam1@gmail.com>
2023-01-15 04:02:18 +0100

    Double buffer added

--- main/animation.cpp
+++ main/animation.cpp
@@ -1,24 +1,29 @@
-g++ -o xpng.out xpng.cpp -lX11 -lXpm -g//compile - 
+// Compile - g++ -o xpng.out xpng.cpp -lX11 -lXpm -lXext -g
 
-#include <iostream>
+#include <stdio.h>
+#include <cstdio>
 #include <X11/Xlib.h>
 #include <X11/Xutil.h>
+#include <X11/extensions/Xdbe.h>
 #include <X11/xpm.h>
 #include <unistd.h>
 
-int main(int argc, char *argv[]) {
+int main(int argc, char *argv[])
+{
     Display *display;
     Window window;
     XEvent event;
     int screen;
     XpmAttributes attribs;
     Pixmap pixmap;
+    Pixmap pixmap_buffer;
     int x = 150;
     int y = 150;
 
     // Open the X11 display
     display = XOpenDisplay(NULL);
-    if (display == NULL) {
+    if (display == NULL)
+    {
         printf("Cannot open display\n");
         return 1;
     }
@@ -38,13 +43,15 @@ int main(int argc, char *argv[]) {
     // Set the XpmSize attribute for the XpmReadFileToPixmap function
     attribs.valuemask = XpmSize;
 
-     // Loop through a series of XPM image files
+    // Create the off-screen buffer
+    pixmap_buffer = XCreatePixmap(display, window, 500, 500, DefaultDepth(display, screen));
 
+    // Loop through a series of XPM image files
     while (true)
     {
         for (int i = 1; i <= 3; i++)
         {
-            char filename[256];
+           char filename[256];
             sprintf(filename, "/home/matvey/Descargas/mech%d.xpm", i);
             XpmReadFileToPixmap(display, window, filename, &pixmap, NULL, &attribs);
             XImage *image = XGetImage(display, pixmap, 0, 0, attribs.width, attribs.height, AllPlanes, ZPixmap);
@@ -59,11 +66,12 @@ int main(int argc, char *argv[]) {
          XClearArea(display, window, x, y, attribs.width, attribs.height, False);
         x = x + 10;
 
-        // Force display to be updated
+       
     }
+     // Force display to be updated
     XFlush(display);
 
-    // Close the display
     XCloseDisplay(display);
+
     return 0;
 }
